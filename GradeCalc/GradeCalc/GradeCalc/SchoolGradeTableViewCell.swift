35d7633a81144bbae2cecba6c48f7e21aea314fd
gusProgram <guspetito@gmail.com>
2018-07-11 13:39:35 -0700

    Set up initial table cell

---
+++ GradeCalc/GradeCalc/GradeCalc/SchoolGradeTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SchoolGradeTableViewCell.swift
+//  GradeCalc
+//
+//  Created by Gus Petito on 7/11/18.
+//  Copyright © 2018 Neil Dhote, Gus Petito. All rights reserved.
+//
+
+import UIKit
+
+class SchoolGradeTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
