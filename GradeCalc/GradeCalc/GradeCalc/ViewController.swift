723835d86c9da583df9df086fe1ed24d85cf8107
Nyotronix <40622092+Nyotronix@users.noreply.github.com>
2018-07-11 01:57:09 -0500

    Add files via upload

---
+++ GradeCalc/GradeCalc/GradeCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GradeCalc
+//
+//  Created by Neil Dhote on 7/10/18.
+//  Copyright © 2018 Neil Dhote, Gus Petito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
