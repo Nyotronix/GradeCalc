c617ef2890f7de31d1a00d474bdfcb2c761c4ac0
gusProgram <guspetito@gmail.com>
2018-07-11 19:35:43 -0700

    Connecting data from table to page, warning very broken

--- GradeCalc/GradeCalc/GradeCalc/SchoolViewController.swift
+++ GradeCalc/GradeCalc/GradeCalc/SchoolViewController.swift
@@ -11,23 +11,18 @@ import UIKit
 class SchoolViewController: UIViewController {
     
     //MARK: Properties
+    var schoolName: String = ""
     
     //MARK: Outlets
+    @IBOutlet weak var ClassName: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        ClassName.text = schoolName
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- GradeCalc/GradeCalc/GradeCalc/ViewController.swift
+++ GradeCalc/GradeCalc/GradeCalc/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    //MARK: IBOutlets
+    @IBOutlet var tableView: UITableView!
+    
     struct MyData{
         var firstRowLabel: String
         var secondRowLabel: String
@@ -17,26 +20,38 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var tableData: [MyData] = []
     
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        //The amount of rows we want in our table
-        return(tableData.count)
-    }
-
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SchoolCell") as! SchoolTableViewCell
-        cell.ClassName.text = tableData[indexPath.row].firstRowLabel
-        cell.Grade.text = tableData[indexPath.row].secondRowLabel
-        return(cell)
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         tableData = [
             MyData(firstRowLabel: "Discrete Math", secondRowLabel: "56%"),
             MyData(firstRowLabel: "US History", secondRowLabel: "33%"),
             MyData(firstRowLabel: "Sculpting", secondRowLabel: "12%")
         ]
+    }
+    
+    //MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let guest = segue.destination as! SchoolViewController
+        guest.schoolName = tableData[tableView.indexPathForSelectedRow?.row as! Int].firstRowLabel
+    }
+    
+    //MARK: UITableViewDelegate Methods
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        //The amount of rows we want in our table
+        return(tableData.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SchoolCell") as! SchoolTableViewCell
+        cell.ClassName.text = tableData[indexPath.row].firstRowLabel
+        cell.Grade.text = tableData[indexPath.row].secondRowLabel
+        return(cell)
     }
 
 }
